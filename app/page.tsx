04233e7fb63c03d582a75ed97ac5c17755006997
marile.lin <marile.lin@viseo.com>
2023-09-25 14:17:48 +0200

    add basic next ts app

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import { Layout } from '@/components'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="overflow-hidden">
+      < Layout />
+    </main>
+  )
+}
---
+++ components/Layout.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+const Layout = () => {
+    return (
+        <div>
+            Layout
+        </div>
+    )
+}
+
+export default Layout
