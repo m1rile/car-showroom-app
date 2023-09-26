20cd54458993fcbec340aeeac7b0e94fecb2edbc
m1rile <marile-lin@hotmail.fr>
2023-09-26 16:56:51 +0200

    wip components of main layout

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,7 +1,19 @@
+import { CustomButtonProps } from '@/types';
 import Image from 'next/image'
 
-const CustomButton = () => {
-    return <button disabled={false} type={'button'}></button>
+const CustomButton = ({ title, containerStyles, handleClick}: CustomButtonProps) => {
+    return (
+        <button
+            disabled={false}
+            type={'button'}
+            className={`custom-btn ${containerStyles}`}
+            onClick={handleClick}
+        >
+            <span className={`flex-1`}>
+                {title}
+            </span>
+        </button>
+    )
 }
 
 export default CustomButton
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,11 @@
+"use client";
+
 import Image from 'next/image'
 import CustomButton from './CustomButton'
 
 const Layout = () => {
+    const handleScroll = () => {}
+
     return (
         <div className="layout">
             <div className="flex-1 pt-36 padding-x">
@@ -14,7 +18,21 @@ const Layout = () => {
                     booking cars.
                 </p>
 
-                <CustomButton />
+                <CustomButton 
+                title="Explore Cars" 
+                containerStyles="bg-primary-blue text-white rounded-full mt-10"
+                handleClick={handleScroll} 
+                />
+
+                <div className='layout__image-container'>
+                    <div className='layout__image'>
+                        <Image src="/layout.png" alt='layout' fill className='object-contain' />
+                    </div>
+
+                    <div className='layout__image-overlay'>
+                        
+                    </div>
+                </div>
             </div>
         </div>
     )
