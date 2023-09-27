f3e828aec87ea81170676b6db5dfece5a25de355
m1rile <marile-lin@hotmail.fr>
2023-09-27 17:26:37 +0200

    wip car catalogue with api data

---
+++ utils/index.ts
@@ -0,0 +1,34 @@
+export async function fetchCars() {
+    const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla'
+
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': process.env.API_KEY ?? "",
+            'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com',
+        }
+    }
+
+    try {
+        const response = await fetch(url, options)
+        return response.json()
+    } catch (error) {
+        console.error(error)
+        return null
+    }
+}
+
+export function calculateCarRent(city_mpg: number, year: number) {
+    const basePricePerDay = 50 // Base rental price per day in dollars
+    const mileageFactor = 0.1 // Additional rate per mile driven
+    const ageFactor = 0.05 // Additional rate per year of vehicle age
+
+    // Calculate additional rate based on mileage and age
+    const mileageRate = city_mpg * mileageFactor
+    const ageRate = (new Date().getFullYear() - year) * ageFactor
+
+    // Calculate total rental rate per day
+    const rentalRatePerDay = basePricePerDay + mileageRate + ageRate
+
+    return rentalRatePerDay.toFixed(0)
+}
