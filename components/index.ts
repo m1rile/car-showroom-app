dc38ae89946024b3fa7470f768dbb765009fdff4
m1rile <marile-lin@hotmail.fr>
2023-09-27 15:25:15 +0200

    add search bar for cars catalogue

--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,8 @@ import Layout from './Layout'
 import CustomButton from './CustomButton'
 import Navbar from './Navbar'
 import Footer from './Footer'
+import SearchBar from './SearchBar'
+import CustomFilter from './CustomFilter'
+import SearchManufacturer from './SearchManufacturer'
 
-export { Layout, CustomButton, Navbar, Footer }
+export { Layout, CustomButton, Navbar, Footer, SearchBar, CustomFilter, SearchManufacturer }
